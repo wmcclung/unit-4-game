d99110341c44ca357b3d8fc839655222d7f5513e
wmcclung <username@users.noreply.github.com>
2018-10-02 00:20:45 -0500

    updated logic

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # unit-4-game
+Try to reach the correct score by clicking the various animal crystals. They each have a random amount which resets each round. If you get the exact score you win. If not you lose. Have fun and try your best
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,124 +1,160 @@
-//variables needed//
 
-var gameNumber;
-var wins = 0;
-var losses = 0;
-var score = 0;
-var gems= [0,0,0,0];
+// This event will occur when the DOM has finished loading
+$(document).ready(function() {
 
-//Functions needed//
-
-//set gamenumber between 19 and 120//
-
-function randomGameNumber () {
-
-    gameNumber = Math.floor(Math.random() * 120) + 19;
-
-}
-$(".target-number").text(gameNumber);
-//set crystal values to between 1-12//
-for(var i = 0; i < gems.length; i++) {
-    gems[i] = (Math.floor(Math.random() * 12) + 1);
-}
-
-
-   $(".col-md-3 dog").click(function(){
-    var gemScore = gems[0];
     
-    score += gemScore;
+    // pick from numbers 19 - 120
+    var targetNumber = Math.floor(Math.random()*101+19);
+    // 2. The target number will be displayed
+    $("#random_number").text(targetNumber);
+
+    // Counter variable will start off at 0
+    var counter = 0;
+
+    // random numbers for crystal picking number from 1 - 12
     
-
-    $(".finalScore").text(score);
-    win();
-    lose();
-   });
-   
-
-   $(".pig").click(function(){
-    var gemScore = gems[1];
+    var dog = Math.floor(Math.random()*11+1);
     
-    score += gemScore;
+    var swan = Math.floor(Math.random()*11+1);
     
-
-    $(".finalScore").text(score);
-    win();
-    lose();
-   });
-   
-
-   $(".swan").click(function(){
-    var gemScore = gems[2];
+    var pig = Math.floor(Math.random()*11+1);
     
-    score += gemScore;
+    var fish = Math.floor(Math.random()*11+1);
+
+    // Variables for score
+    var userScore = 0;
+    var wins = 0;
+    var losses = 0;
+
+    // initial metrics
+    $("#totalScore").text(userScore);
+    $("#userWins").text(wins);
+    $("#userLosses").text(losses);
     
-
-    $(".finalScore").text(score);
-    win();
-    lose();
-   });
-
-   
-   $(".fish").click(function(){
-    var gemScore = gems[1];
-    
-    score += gemScore;
-    
-
-    $(".finalScore").text(score);
-    win();
-    lose();
-   });
-
-
-   //The player wins if their total score matches the random number from the beginning of the game.
-   //create conditions: if total score equals to the computer generated number, alert win
-   function win(){
-    if(totalScore === number){
-    alert("You Win!");
-    wins++;
-    $(".winCount").text(wins);
-    reset();
+    // resets variables
+    function reset() {
+        targetNumber = Math.floor(Math.random()*101+19);
+        // reset after game
+        $("#random_number").text(targetNumber);
+        userScore = 0;
+        //restart
+        $("#totalScore").text(userScore);
+        num1 = Math.floor(Math.random()*11+1);
+        num2 = Math.floor(Math.random()*11+1);
+        num3 = Math.floor(Math.random()*11+1);
+        num4 = Math.floor(Math.random()*11+1);
     }
-   }
 
-   //The player loses if their score goes above the random number.
-   //create condition for loss if the total scoree is not equal to the computer generated number. Alert loss
-   function lose(){
-    if(totalScore > number) {
-      alert("You Lose!");
-      loss++;
-      $(".lossCount").text(loss);
-      reset();
-    }
-   }
-   //The game restarts whenever the player wins or loses.
-   //reset the game after each alert of win/loss
+    // click function dog
+    $("#row4col1").on("click", "#dog", function() {
 
-    //--Reset--
-     function reset(){
-      number = 0; 
-      totalScore = 0; 
-      ranNum = [0,0,0,0]
+        // add dog
+        userScore = userScore + dog;
 
-      number = (Math.floor(Math.random() * 101) + 19);
-      console.log(number);
+        // update score
+        $("#totalScore").text(userScore);
 
-     $(".computerNum").text(number);
-    
-     for(var i = 0; i < ranNum.length; i++) {
-      ranNum[i] = (Math.floor(Math.random() * 12) + 1);
-       console.log(ranNum);
-       $(".finalScore").text(totalScore);
-     }
-     }
+        
 
-    //When the game begins again, the player should see a new random number. 
-    //Also, all the crystals will have four new hidden values. 
-    //Of course, the user's score (and score counter) will reset to zero.
+        // If I win
+        if (userScore === targetNumber) {
+            alert("You won!");
+            wins++;
+            $("#userWins").text(wins);
+            reset();
+        }
 
-   //The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
-   //store the number of wins or losses to correct variable
+        // If i lose
+        else if (userScore > targetNumber) {
+            alert("You lose!");
+            losses++;
+            $("#userLosses").text(losses);
+            reset();
+        }
+    });
 
-  //The random number shown at the start of the game should be between 19 - 120.
+    // click function swan
+    $("#row4col1").on("click", "#swan", function() {
 
-  //Each crystal should have a random hidden value between 1 - 12.
+        // add swan
+        userScore = userScore + swan;
+
+        // update score
+        $("#totalScore").text(userScore);
+
+        
+
+        // If I win
+        if (userScore === targetNumber) {
+            alert("You won!");
+            wins++;
+            $("#userWins").text(wins);
+            reset();
+        }
+
+        // If I lose
+        else if (userScore > targetNumber) {
+            alert("You lose!");
+            losses++;
+            $("#userLosses").text(losses);
+            reset();
+        }
+    });
+
+    // click function pig
+    $("#row4col1").on("click", "#pig", function() {
+
+        // add pig
+        userScore = userScore + pig;
+
+        // update score
+        $("#totalScore").text(userScore);
+
+     
+
+        // If I win
+        if (userScore === targetNumber) {
+            alert("You won!");
+            wins++;
+            $("#userWins").text(wins);
+            reset();
+        }
+
+        // If I lose
+        else if (userScore > targetNumber) {
+            alert("You lose!");
+            losses++;
+            $("#userLosses").text(losses);
+            reset();
+        }
+    });
+
+    // click function fish
+    $("#row4col1").on("click", "#fish", function() {
+
+        // add fish
+        userScore = userScore + fish;
+
+        // update score
+        $("#totalScore").text(userScore);
+
+        
+
+        // If I win
+        if (userScore === targetNumber) {
+            alert("You won!");
+            wins++;
+            $("#userWins").text(wins);
+            reset();
+        }
+
+        // If I lose
+        else if (userScore > targetNumber) {
+            alert("You lose!");
+            losses++;
+            $("#userLosses").text(losses);
+            reset();
+        }
+    });
+
+});
